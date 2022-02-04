16db7cbe44e1d3e15bf6b8f5c9ffc661d692ff31
Himalf <himal.fullel15@gmail.com>
2022-02-04 23:36:53 +0545

    ok

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log('Hello World!');
 alert('welcome to my small page have a good day to you watch this video,see this picture and listen this song for your good day we will add new items daily thanks for your coorporation.');
+alert('due to some technical error we cannot display video');
